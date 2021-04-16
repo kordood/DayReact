c38f2d979b5be839a4a6cc3304c5ff8809c18627
kordood <gigacms@gmail.com>
2021-04-16 12:20:13 +0000

    test button click as count up

--- hello/src/App.js
+++ hello/src/App.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 
 class App extends Component {
 	state = {
-		hello: 'hello app jr!!'
+		count: 0
 	};
 	
-	handleChange = () => {
+	countUp = () => {
 		this.setState({
-			hello: 'bye app js!'
+			count: this.state.count + 1
 		});
 	};
 
 	render() {
 		return (
 			<div className="App">
-				<div>{this.state.hello}</div>
-				<button onClick={this.handleChange}>click Me!</button>
+				<div>{this.state.count}</div>
+				<button onClick={this.countUp}>count up!</button>
 			</div>
 		);
 	}
