9845351c57060c35cd287a6f97036751404ebf8b
kordood <gigacms@gmail.com>
2021-04-16 12:29:18 +0000

    basic example of props

--- hello/src/App.js
+++ hello/src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 class App extends Component {
 	state = {
-		count: 0
+		count: 0,
 	};
 	
-	countUp = () => {
+	handleChange = () => {
 		this.setState({
 			count: this.state.count + 1
 		});
@@ -14,8 +14,16 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<div>{this.state.count}</div>
-				<button onClick={this.countUp}>count up!</button>
+				<div className="props">
+					{/* Props가 들어가는 부분입니다! */}
+					<span>{this.props.message}</span>
+				</div>
+
+				<div className="state">
+					{/* State가 들어가는 부분입니다! */}
+					{this.state.count}
+					<button onClick={this.handleChange}> Click Me ! </button>
+				</div>
 			</div>
 		);
 	}
--- hello/src/index.js
+++ hello/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-		<App />,
+		<App message="Hello Message" />,
 		document.getElementById('root')
 );
 
