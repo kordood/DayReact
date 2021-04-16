95a46f5cb0b967a90ddc4b3381e6cf9566803daf
kordood <gigacms@gmail.com>
2021-04-16 12:37:53 +0000

    Implement extends class

--- hello/src/App.js
+++ hello/src/App.js
@@ -14,19 +14,42 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<h3>index Props</h3>
 				<div className="props">
 					{/* Props가 들어가는 부분입니다! */}
 					<span>{this.props.message}</span>
 				</div>
 
+				<h3>State</h3>
 				<div className="state">
 					{/* State가 들어가는 부분입니다! */}
 					{this.state.count}
 					<button onClick={this.handleChange}> Click Me ! </button>
+				</div>
+
+				<h3>App Props</h3>
+				<div className="inside-app-props">
+					<InsideApp
+						count={this.state.count}
+						handleChange={this.handleChange}
+					/>
 				</div>
 			</div>
 		);
 	}
 }
 
+class InsideApp extends Component {
+	render() {
+		return (
+			<div>
+				{this.props.count}
+				<button onClick={this.props.handleChange}>click Me !</button>
+			</div>
+		);
+	}
+}
+
+
 export default App;
+
